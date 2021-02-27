ee557609594b2a42430a19aa5622f90e51433913
fengnan <fengnmail@foxmail.com>
2021-02-27 14:05:15 +0800

    codepen to app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
+  <h1 class="text-primary"><fa icon="shopping-cart" />shopping cart</h1>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'Home'
+}
 </script>
