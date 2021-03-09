17d750e05f8d4bd369b2cf09631598fb02188a30
fengnan <fengnmail@foxmail.com>
2021-03-09 10:15:00 +0800

    the fourth step

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Checkout">Checkout</router-link>
-  </div>
+  <navbar :cart="cart" />
   <div class="container">
-    <router-view />
+    <router-view :cart="cart" @addToCart="addToCart" />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  data: function() {
+    return {
+      cart: [],
+      displayCart: false
+    }
+  },
+  components: {
+    Navbar
+  },
+  methods: {
+    addToCart(product) {
+      this.cart.push(product)
+      if (this.cartTotal >= 100) {
+        this.salesBtn = 'btn-danger'
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 $primary: #6f42c1;
 @import 'node_modules/bootstrap/scss/bootstrap';
-
-.dropdown-clip {
-  overflow: hidden;
-}
-
-.dropdown-enter-active,
-.dropdown-leave-active {
-  transition: all 0.5s ease-in-out;
-  transform: auto;
-}
-
-.dropdown-enter-from,
-.dropdown-leave-to {
-  opacity: 0;
-  transform: translateY(-300px);
-}
 
 .products-enter-active,
 .products-leave-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,6 @@
 <template>
   <!-- <div class="home"></div> -->
   <!-- <h1 class="text-primary"><fa icon="shopping-cart" />shopping cart</h1> -->
-  <nav class="navbar navbar-light sticky-top mr-3">
-    <div
-      v-if="cart.length"
-      class=" w-100 navbar-text ml-auto d-flex justify-content-end position-relative"
-    >
-      <div
-        class="mr-auto d-flex align-items-end flex-column bd-highlight mb-3 position-absolute"
-      >
-        <div class="mb-2">
-          <span class="font-weight-bold bg-white"
-            ><curr :amt="cartTotal"></curr
-          ></span>
-          <button
-            @click="displayCart = !displayCart"
-            class="btn btn-sm btn-success ml-3"
-            id="cartDropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <fa icon="shopping-cart" />
-            {{ cart.length }}
-          </button>
-        </div>
-        <div class="dropdown-clip">
-          <transition name="dropdown">
-            <div
-              v-if="displayCart"
-              class="list-group"
-              aria-labelledby="cartDropdown"
-            >
-              <div
-                v-for="(item, index) in cart"
-                :key="index"
-                class="list-group-item d-flex justify-content-between"
-              >
-                <div>{{ item.name }}</div>
-                <div class="ml-3 font-weight-bold">
-                  <curr :amt="item.price"></curr>
-                </div>
-              </div>
-            </div>
-          </transition>
-        </div>
-      </div>
-    </div>
-  </nav>
 
   <section class="container">
     <label for="max-price" class="form-label h2">Max Price (${{ max }})</label>
@@ -62,9 +16,6 @@
       max="130"
     />
 
-    <custom-alert type="danger" close="true" v-if="cartTotal > 100">
-    </custom-alert>
-
     <transition-group name="products" appear>
       <div
         v-for="item in filteredProducts"
@@ -79,8 +30,6 @@
 </template>
 
 <script>
-import Curr from '@/components/Curr'
-import CustomAlert from '@/components/CustomAlert'
 import Product from '@/components/Product'
 
 export default {
@@ -94,8 +43,6 @@ export default {
     }
   },
   components: {
-    Curr,
-    CustomAlert,
     Product
   },
   created() {
@@ -108,17 +55,6 @@ export default {
   computed: {
     filteredProducts() {
       return this.products.filter(item => item.price < this.max)
-    },
-    cartTotal() {
-      return this.cart.reduce((inc, item) => Number(item.price) + inc, 0)
-    }
-  },
-  methods: {
-    addToCart(product) {
-      this.cart.push(product)
-      if (this.cartTotal >= 100) {
-        this.salesBtn = 'btn-danger'
-      }
     }
   }
 }
