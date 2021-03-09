f0b83ae43c723dc91178a6fd782d91ff782ad647
fengnan <fengnmail@foxmail.com>
2021-03-09 11:23:51 +0800

    the fifth step

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,34 +3,14 @@
   <!-- <h1 class="text-primary"><fa icon="shopping-cart" />shopping cart</h1> -->
 
   <section class="container">
-    <label for="max-price" class="form-label h2">Max Price (${{ max }})</label>
-    <div class="badge bg-success ml-3">
-      results: {{ filteredProducts.length }}
-    </div>
-
-    <input
-      v-model.number="max"
-      type="range"
-      class="form-range"
-      min="0"
-      max="130"
-    />
-
-    <transition-group name="products" appear>
-      <div
-        v-for="item in filteredProducts"
-        :key="item.id"
-        id="item-list"
-        class="row align-items-center"
-      >
-        <product :item="item" @add-to-cart="addToCart"></product>
-      </div>
-    </transition-group>
+    <ranger-selector :products="filteredProducts" v-model="max" />
+    <product-list :products="filteredProducts" />
   </section>
 </template>
 
 <script>
-import Product from '@/components/Product'
+import ProductList from '@/components/ProductList'
+import RangerSelector from '@/components/RangerSelector'
 
 export default {
   name: 'Home',
@@ -38,12 +18,12 @@ export default {
     return {
       max: 50,
       cart: [],
-      displayCart: false,
       products: []
     }
   },
   components: {
-    Product
+    RangerSelector,
+    ProductList
   },
   created() {
     fetch('https://hplussport.com/api/products/order/price')
@@ -54,7 +34,7 @@ export default {
   },
   computed: {
     filteredProducts() {
-      return this.products.filter(item => item.price < this.max)
+      return this.products.filter(item => item.price < Number(this.max))
     }
   }
 }
